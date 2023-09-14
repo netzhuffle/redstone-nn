1866290d1f36ca6b21f75bb2511eb08d04f39f14
Jannis Grimm <j@jannis.rocks>
2023-09-14 07:10:39 +0200

    Add eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  root: true,
+};
